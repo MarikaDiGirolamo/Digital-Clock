4347cb04b25d1f92938027fbf18baca10db0760d
MarikaDiGirolamo <marikadigirolamo@gmail.com>
2023-10-19 23:50:41 +0200

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,37 +1,71 @@
+// Imposta un intervallo per chiamare la funzione displayTime ogni secondo (1000 millisecondi).
 setInterval(displayTime, 1000);
 
+// La funzione displayTime viene chiamata all'inizio e poi a intervalli regolari.
 function displayTime() {
-
+    // Ottieni la data corrente.
     const date = new Date();
 
-    let h = date.getHours();
-    let m = date.getMinutes();
-    let s = date.getSeconds();
-    let weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-    let today = weekDay[date.getDay()];
-    let mm = date.toLocaleString("default", {
-        month: "long"
-    });
-    let yy = date.getFullYear();
-    let period = "PM";
+    // Formatta l'ora in un formato orario 12 ore.
+    const h = formatHour(date.getHours());
 
-    if (h > 12) {
-        h-= 12;
-        period = "PM";
-    }
+    // Formatta i minuti e i secondi aggiungendo uno zero iniziale se necessario.
+    const m = formatTimeUnit(date.getMinutes());
+    const s = formatTimeUnit(date.getSeconds());
 
-    if (h === 0) {
-        h = 12;
-        period = "AM";
-    }
+    // Ottieni il giorno della settimana, il nome del mese e l'anno corrente.
+    const today = getDayOfWeek(date);
+    const mm = getMonthName(date);
+    const yy = date.getFullYear();
 
-    h = h < 10 ? "0" + h : h;
-    m = m < 10 ? "0" + m : m;
-    s = s < 10 ? "0" + s : s;
+    // Ottieni il periodo (AM o PM).
+    const period = getPeriod(date);
 
-    let time = h + ":" + m + ":" + s + period;
+    // Componi l'orario completo in un'unica stringa.
+    const time = `${h}:${m}:${s}`;
 
-    document.getElementById('Digital_Clock').innerHTML = time + " " + today + " " + mm + " " + yy;
+    // Ottieni il formato della data "oggi data mese anno".
+    const formattedDate = `${date.getDate()} ${mm} ${yy}`;
 
+    // Ottieni l'elemento HTML con l'ID 'Digital_Clock' e aggiorna il suo contenuto.
+    const digitalClock = document.getElementById('Digital_Clock');
+    digitalClock.innerHTML = `${time} ${period}`;
+
+    const hourClock = document.getElementById('Hour_Clock');
+    hourClock.innerHTML = `${today} ${formattedDate}`;
 }
+
+// Questa funzione formatta l'ora in un formato orario 12 ore.
+function formatHour(hour) {
+    if (hour > 12) {
+        hour -= 12;
+    }
+    if (hour === 0) {
+        hour = 12;
+    }
+    return formatTimeUnit(hour);
+}
+
+// Questa funzione aggiunge uno zero iniziale se l'unità è inferiore a 10.
+function formatTimeUnit(unit) {
+    return unit < 10 ? "0" + unit : unit;
+}
+
+// Questa funzione restituisce il nome del giorno della settimana.
+function getDayOfWeek(date) {
+    const weekDays = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
+    return weekDays[date.getDay()];
+}
+
+// Questa funzione restituisce il nome del mese.
+function getMonthName(date) {
+    return date.toLocaleString("default", { month: "long" });
+}
+
+// Questa funzione restituisce il periodo (AM o PM) in base all'ora corrente.
+function getPeriod(date) {
+    return date.getHours() //>= 12 ? "PM" : "AM";
+}
+
+// Chiamiamo la funzione displayTime all'avvio per mostrare l'orario corrente.
 displayTime();
